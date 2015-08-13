cd58aac0beb30f46c276ebffd1268149d6b28c8b
Jonathan Lam <jonlam@gmail.com>
2015-08-13 00:44:21 -0500

    develop checkpoint

--- js/placeOfInterests.js
+++ js/placeOfInterests.js
@@ -1,14 +1,24 @@
 // 1. look at prototype again
 // 2. namespace
 
+var Location = function(lat, lon) {
+	this.lat = lat;
+	this.lon = lon;	
+};
+
 var Place = function(title, subtitle, location) {
 	this.title = title;
 	this.subtitle = subtitle;
-	this.location = location;	
+	this.location = location;
+	
+	this.PrettyPrintCoords = function() {
+		return this.location.lat + ", " + this.location.lon;
+	};	
 };
+
 
 var Places = [];
 
 for(var i = 0; i < 10; i++) {
-	Places.push(new Place("title" + i, "subtitle" + i, "location" + i));
+	Places.push(new Place("title" + i, "subtitle" + i, new Location(29.7604 + i/100, 95.3698 + i/100 )));
 }
